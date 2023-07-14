0c060c0dc34647c8951dc466b582c46e7521ded7
BeeAwwad <aosalihu@gmail.com>
2023-07-14 08:33:09 +0100

    lesson 3

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = "mongodb+srv://awwad:qYE7tVRgRrigAHmu@node-auth.f30ke14.mongodb.net/node-auth";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.use(authRoutes);
+
+// 404 page
+app.use((req, res) => res.render('404', { title: '404' }))
